d17e57e622b6960aa4e7555d3d40967af4089519
Manish Manwani <manishmanwani73@gmail.com>
2024-01-15 16:55:50 +1030

    :lipstick: (card wrapper): added basic auth form component wrapper elements

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,14 @@
+import { CardWrapper } from './CardWrapper';
+
+export function LoginForm() {
+    return (
+        <CardWrapper
+            headerLabel="Welcome Back!"
+            backButtonLabel="Don't have an Account?"
+            backButtonHref="/auth/register"
+            showSocial
+        >
+            LoginForm
+        </CardWrapper>
+    );
+}
