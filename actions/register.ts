f379904625b56c740f5f1fc28536ed3cadaacca5
Manish Manwani <manishmanwani73@gmail.com>
2024-02-10 16:23:47 +1030

    :sparkles: (email register): users can now register with email, acc …

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 'use server';
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -8,6 +10,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: 'Invalid Fields' };
     }
+
+    const { email, password, name } = validatedFields.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (existingUser) {
+        return { error: 'Email already in use!' };
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+        },
+    });
+
+    // TODO: Send email verification token
 
     return { success: 'Email sent!' };
 };
