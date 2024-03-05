9b47293e3e3a79c8bb7304a053603e116660f39d
ManishManwani36 <manishmanwani73@gmail.com>
2024-03-05 11:51:50 +1030

    :sparkles: (server client page): added pages with server and client m…

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,7 @@
+import UserInfo from '@/components/UserInfo';
+import { currentUser } from '@/lib/auth';
+
+export default async function ServerPage() {
+  const user = await currentUser();
+  return <UserInfo user={user} label="💻 Server Component" />;
+}
